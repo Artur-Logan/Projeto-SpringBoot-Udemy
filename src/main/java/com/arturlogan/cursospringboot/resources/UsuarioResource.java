bb073c1c068f75768bebc81a7a4a807230b943ea
Artur Logan <arturlogan1ferreira@gmail.com>
2022-06-19 21:10:07 -0300

    Service layer, component registration

--- src/main/java/com/arturlogan/cursospringboot/resources/UsuarioResource.java
+++ src/main/java/com/arturlogan/cursospringboot/resources/UsuarioResource.java
@@ -1,19 +1,32 @@
 package com.arturlogan.cursospringboot.resources;
 
 import com.arturlogan.cursospringboot.entitites.Usuario;
+import com.arturlogan.cursospringboot.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioResource {
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping
-    public ResponseEntity<Usuario> findAll(){
-        Usuario usuario = new Usuario(2L,"artur", "12345", "asdfg", "ad");
-        return ResponseEntity.ok().body(usuario);
+    public ResponseEntity<List<Usuario>> findAll(){
+        List<Usuario> usuarioList = usuarioService.findAll();
+        return ResponseEntity.ok().body(usuarioList);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable Long id){
+     Usuario usuario = usuarioService.findById(id);
+     return ResponseEntity.ok().body(usuario);
+    }
 }
---
+++ src/main/java/com/arturlogan/cursospringboot/services/UsuarioService.java
@@ -0,0 +1,25 @@
+package com.arturlogan.cursospringboot.services;
+
+import com.arturlogan.cursospringboot.entitites.Usuario;
+import com.arturlogan.cursospringboot.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository  usuarioRepository;
+
+    public List<Usuario> findAll(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario findById(Long id){
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        return usuario.get();
+    }
+}
